374b0b44cfdffa1683fb4b40535dcd684238780d
Koen Bloemen <koen.bloemen@pxl.be>
2024-11-08 11:33:40 +0100

    main

---
+++ Labo-9---MenuSliderBrushes/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Labo_9___MenuSliderBrushes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
