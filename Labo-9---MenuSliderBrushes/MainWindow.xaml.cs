b43884b811efd5935ceb7d95f9abc9f2183a5fa7
Koen Bloemen <koen.bloemen@pxl.be>
2024-11-08 11:47:41 +0100

    final

--- Labo-9---MenuSliderBrushes/MainWindow.xaml.cs
+++ Labo-9---MenuSliderBrushes/MainWindow.xaml.cs
@@ -20,5 +20,46 @@ namespace Labo_9___MenuSliderBrushes
         {
             InitializeComponent();
         }
+
+        private void numberOneMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            numberOneSlider.Value = 2;
+        }
+
+        private void numberTwoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            numberTwoSlider.Value = 2;
+        }
+
+        private void closeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Afsluiten", "Bent u zeker dat u wil afsluiten?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.No)
+                e.Cancel = true;
+        }
+
+        private void numberTwoSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberTwoTextBox.Text = numberTwoSlider.Value.ToString();
+        }
+
+        private void numberOneSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberOneTextBox.Text = numberOneSlider.Value.ToString();
+        }
+
+        private void numberOneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            numberOneSlider.Value = int.Parse(numberOneTextBox.Text);
+        }
+
+        private void numberTwoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            numberTwoSlider.Value = int.Parse(numberTwoTextBox.Text);
+        }
     }
 }
